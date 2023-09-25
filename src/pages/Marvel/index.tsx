dfe80f9233e9073e263a4512ed9b29810b74226e
Anaclaraduartem <anaclaraduartemiranda03@gmail.com>
2023-09-25 12:26:39 -0300

    criei o projeto
    coloquei logo

---
+++ src/pages/Marvel/index.tsx
@@ -0,0 +1,12 @@
+import styles from './Marvel.module.scss';
+import logoo from '../../assets/marvel/logoo.jpg';
+
+export default function Marvel(){
+    return(
+        <main className={styles.bg}>
+            <nav className={styles.menu}>
+                
+            </nav>
+        </main>
+    );
+}
