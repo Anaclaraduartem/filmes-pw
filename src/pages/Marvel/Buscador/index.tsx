ebf7b3033886dda530e70ebea3b191ee3636045b
Ana Clara <99971659+Anaclaraduartem@users.noreply.github.com>
2023-10-05 10:30:44 -0300

    Update index.tsx

--- src/pages/Marvel/Buscador/index.tsx
+++ src/pages/Marvel/Buscador/index.tsx
@@ -1,4 +1,10 @@
-export default function Buscador(){
+Interface Props{
+busca:string,
+    setBusca:React.Dispatch<React.SetStateAction<string>>
+
+}
+
+export default function Buscador({busca,setBusca}: Props){
     return(
         <div>Buscador</div>
     )
