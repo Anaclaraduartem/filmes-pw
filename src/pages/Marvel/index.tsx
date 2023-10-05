3368c19d27351a42dfbc88bca7925210bc60bfd6
Ana Clara <99971659+Anaclaraduartem@users.noreply.github.com>
2023-10-05 10:26:25 -0300

    Update index.tsx

--- src/pages/Marvel/index.tsx
+++ src/pages/Marvel/index.tsx
@@ -2,6 +2,8 @@ import styles from './Marvel.module.scss';
 import logoo from '../../assets/marvel/logoo.jpg';
 
 export default function Marvel(){
+    const [busca,setBusca] = useState("");
+    
     return(
         <main className={styles.bg}>
             <nav className={styles.menu}>
@@ -9,7 +11,8 @@ export default function Marvel(){
             </nav>
          
 <section>
-    
+    <h3>Filmes</h3>
+    <Buscador busca ={busca} setBusca = {setBusca}/>
 </section>
         </main>
     );
